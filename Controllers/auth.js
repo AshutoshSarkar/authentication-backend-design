579a50efccefa3153ff300d112781f6e4fc6d6ac
Ashutosh Sarkar <ashut.sarkar8187@gmail.com>
2024-07-18 23:40:50 +0530

    Completed password encryption

--- Controllers/auth.js
+++ Controllers/auth.js
@@ -1,7 +1,41 @@
-const login = async (req, res) => {
-    res.send("login");
+import User from "../Models/userModel.js";
+import bcrypt from "bcrypt";
+
+export const signup = async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists",
+      });
+    }
+
+    //secure the password
+    let hashedPassword;
+    try {
+      hashedPassword = await bcrypt.hash(password, 10);
+    } catch (err) {
+      return res
+        .status(500)
+        .json({ success: false, message: "Internal server error" });
+    }
+
+    //create new entry for user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      role,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
 };
 
-const signup = async (req, res) => {};
-
-export { login, signup };
+export const login = async (req, res) => {};
